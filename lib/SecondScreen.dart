848d0fa4fd0eac47dbf3e142a142dc0a442885f1
abhisheksps <abhishek_sharma@softprodigy.com>
2022-03-22 11:22:06 +0530

    Initial commit

---
+++ lib/SecondScreen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:numeric_keyboard/numeric_keyboard.dart';
+
+class SecondScreen extends StatefulWidget {
+  final String value;
+
+  const SecondScreen({Key? key, required this.value}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return SecondScreenState();
+  }
+}
+
+class SecondScreenState extends State<SecondScreen> {
+  String text = '';
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+
+      appBar: AppBar(
+        title: Text("Second Screen", style: Theme.of(context).textTheme.headline1,),
+
+      ),
+      body: Column(
+        children: [
+          Text(text),
+          NumericKeyboard(
+              onKeyboardTap: _onKeyboardTap,
+              textColor: Colors.red,
+              rightButtonFn: () {
+                setState(() {
+                  text = text.substring(0, text.length - 1);
+                });
+              },
+              rightIcon: Icon(Icons.backspace, color: Colors.red,),
+              leftButtonFn: () {
+                print('left button clicked');
+              },
+              leftIcon: Icon(Icons.check, color: Colors.red,),
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly
+          ),
+        ],
+
+      )
+    );
+
+  }
+
+  _onKeyboardTap(String value) {
+    setState(() {
+      text = text + value;
+    });
+
+}}
