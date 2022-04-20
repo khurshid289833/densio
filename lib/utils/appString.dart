727e4825b27c468338ebefcaf862fb7546cd502c
abhisheksps <abhishek_sharma@softprodigy.com>
2022-04-20 17:43:59 +0530

    home and logout

--- lib/utils/appString.dart
+++ lib/utils/appString.dart
@@ -3,7 +3,7 @@ class AppString{
   ///Sign Up Page///
   static const String SignUp = "Sign Up";
   static const String SignIn = "Sign In";
-  static const String emailAddress = "Email address...";
+  static const String emailAddress = "Email address/Mobile number...";
   static const String password = "Password";
   static const String rememberMe = "Remember me";
   static const String forgotPassword = "Forgot Password?";
